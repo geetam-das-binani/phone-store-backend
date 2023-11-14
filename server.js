021426de13283e100a378beb5db6f3eadc66dde5
Geetam <geetambinani6@gmail.com>
2023-11-14 11:17:41 +0530

    add env port

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 dotenv.config({ path: path.join(__dirname, "./config/config.env") });
+const PORT=process.env.PORT || 3001
 connect(process.env.NAME, process.env.PASSWORD);
 app.use("/", router);
 
-app.listen(process.env.PORT, () =>
+app.listen(PORT, () =>
   console.log(`Server started on port ${process.env.PORT}`)
 );
